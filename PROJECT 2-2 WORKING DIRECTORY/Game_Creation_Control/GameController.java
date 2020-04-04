81040c204b8c6e2a0315eed1d6593cb3c191374a
Seeeeeyo <selimgilon@gmail.com>
2020-04-04 13:32:08 +0200

    Finished the winConditions

--- PROJECT 2-2 WORKING DIRECTORY/Game_Creation_Control/GameController.java
+++ PROJECT 2-2 WORKING DIRECTORY/Game_Creation_Control/GameController.java
@@ -77,9 +77,6 @@ public class GameController {
                     }
                      GuiController.updateGui();
                 }
-
-            handleActionRequest();
-            checkWinConditions();
         }
 }
 
--- PROJECT 2-2 WORKING DIRECTORY/Game_Creation_Control/GameLauncher.java
+++ PROJECT 2-2 WORKING DIRECTORY/Game_Creation_Control/GameLauncher.java
@@ -1,10 +1,10 @@
-//package Game_Creation_Control;
-//
-//public class GameLauncher {
-//    public static void main(String[] args) {
-//        String mapFile = "C:\\Users\\steph\\Desktop\\PROJECT 2-2 WORKING DIRECTORY";
-//        GameController gameController = new GameController(mapFile);
-//        gameController.setup();
-//        gameController.start();
-//    }
-//}
+package Game_Creation_Control;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        String mapFile = "C:\\Users\\steph\\Desktop\\PROJECT 2-2 WORKING DIRECTORY";
+        GameController gameController = new GameController(mapFile);
+        gameController.setup();
+        gameController.start();
+    }
+}
